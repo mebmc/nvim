63da6e7fe1c24984fb336bcb4e8e3f7c646626d4
Ben McDonald <ben@mebmc.uk>
2024-02-17 18:32:51 +0000

    Updare keymaps for terminal and explorer

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -4,3 +4,17 @@ vim.api.nvim_create_autocmd({'BufWinEnter'}, {
   pattern = '*',
   command = 'silent! normal! g`"zv',
 })
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<M-h>', [[<Cmd>TmuxNavigateLeft<CR>]], opts)
+  vim.keymap.set('t', '<M-j>', [[<Cmd>TmuxNavigateDown<CR>]], opts)
+  vim.keymap.set('t', '<M-k>', [[<Cmd>TmuxNavigateUp<CR>]], opts)
+  vim.keymap.set('t', '<M-l>', [[<Cmd>TmuxNavigateRight<CR>]], opts)
+  vim.keymap.set('t', '<M-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
