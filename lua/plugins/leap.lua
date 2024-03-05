41c0185e37f8e50c4be2fc5e106f3ecba4956975
Ben McDonald <ben@mebmc.uk>
2024-03-05 20:28:39 +0000

    Add leap plugin

---
+++ lua/plugins/leap.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        "ggandor/leap.nvim",
+        dependencies = {
+            'tpope/vim-repeat',
+        },
+        event = {
+            "BufReadPre",
+            "BufNewFile"
+        },
+        config = function(_)
+            require('leap').add_default_mappings()
+        end,
+        opts = {},
+    },
+    {
+        "ggandor/flit.nvim",
+        dependencies = {
+            "ggandor/leap.nvim",
+        },
+        event = {
+            "BufReadPre",
+            "BufNewFile"
+        },
+    }
+}
