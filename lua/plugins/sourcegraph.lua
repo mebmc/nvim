8da339bdbcab32681b689f62c4a7e968e9637959
Ben McDonald <ben@mebmc.uk>
2024-03-05 20:32:04 +0000

    Update plugin configs

--- lua/plugins/sourcegraph.lua
+++ lua/plugins/sourcegraph.lua
@@ -13,7 +13,7 @@ return {
         -- If you have a recent version of lazy.nvim, you don't need to add this!
         build = "nvim -l build/init.lua",
         keys = {
-            { "<leader>act", ":CodyToggle<cr>", desc = "Toggle Cody" }
+            { "<leader>aCt", ":CodyToggle<cr>", desc = "Toggle Cody" }
         },
         opts = {}
     },
