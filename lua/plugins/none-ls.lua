1cdde4dbae07dbbdf666668e77f8c44ba0a6a388
Ben McDonald <ben@mebmc.uk>
2024-03-05 20:25:08 +0000

    Update lsp config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,3 +1,11 @@
 return {
-    'nvimtools/none-ls.nvim'
+    'nvimtools/none-ls.nvim',
+    config = function(_, opts)
+        local none_ls = require('null-ls')
+        none_ls.setup(opts)
+
+        none_ls.builtins.diagnostics.yamllint.with({
+            filetypes = { 'yaml', 'yml', 'yaml.ansible' },
+        })
+    end
 }
---
+++ lua/plugins/nvim-mason-null-ls.lua
@@ -0,0 +1,46 @@
+-- https://github.com/jay-babu/mason-null-ls.nvim
+return {
+    "jay-babu/mason-null-ls.nvim",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "nvimtools/none-ls.nvim"
+    },
+    event = {
+        "BufReadPre",
+        "BufNewFile"
+    },
+    opts = {
+        automatic_installation = true,
+        ensure_installed = {
+            "black",
+            "gitlint",
+            "jsonlint",
+            "jq",
+            "rustfmt",
+            -- "semgrep",
+            "shellcheck",
+            "shfmt",
+            -- "stylua",
+            "vale",
+            "yamllint"
+        },
+        handlers = {
+            semgrep = function(source_name, methods)
+                local null_ls = require("null-ls")
+                null_ls.register(
+                    null_ls.builtins.diagnostics.semgrep.with({
+                        args = { "--config", "auto", "-q", "--json", "$FILENAME" },
+                        timeout = 30000,
+                        method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+                    })
+                )
+            end,
+            prettierd = function()
+                local null_ls = require "null-ls"
+                null_ls.register(
+                    null_ls.builtins.formatting.prettierd.with { extra_filetypes = { "markdown", "rmd", "qmd" } }
+                )
+            end,
+        },
+    },
+}
