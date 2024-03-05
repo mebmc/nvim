0e15da3999c71e63e2c4f81bfe5346c8ffd8ba08
Ben McDonald <ben@mebmc.uk>
2024-03-05 20:25:08 +0000

    Update lsp config

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -6,18 +6,39 @@ return {
         --Please make sure you install markdown and markdown_inline parser
         { "nvim-treesitter/nvim-treesitter" }
     },
-    opts = {
-        outline = {
-            keys = {
-                toggle_or_jump = '<CR>'
+    config = function()
+        require("lspsaga").setup({
+            definition = {
+                width = 0.6,
+                height = 0.5,
             },
-            auto_close = true,
-            close_after_jump = true,
-        },
-        lightbulb = {
-            enabled = true,
-            sign = true,
-            virtual_text = false,
-        },
-    },
+            diagnostic = {
+                -- TODO: Make diagnostic float window bigger
+                max_width = 0.8,
+                max_height = 0.8,
+                show_normal_height = 20,
+                max_show_width = 0.9,
+                max_show_height = 0.6,
+                keys = {
+                    quit = { 'c', '<ESC>' }
+                }
+            },
+            hover = {
+                max_width = 0.9,
+                max_height = 0.8,
+            },
+            outline = {
+                keys = {
+                    toggle_or_jump = '<CR>'
+                },
+                auto_close = true,
+                close_after_jump = true,
+            },
+            lightbulb = {
+                enabled = true,
+                sign = true,
+                virtual_text = false,
+            },
+        })
+    end
 }
