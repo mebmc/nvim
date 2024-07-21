8467c6043ac0e762684d906f5d7665550461b2ec
Ben McDonald <ben@mebmc.uk>
2024-07-21 22:23:00 +0100

    Update which-key plugin config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -13,52 +13,33 @@ return {
             separator  = "➜", -- symbol used between a key and it's label
             group      = "", -- symbol prepended to a group
         },
-        -- defaults = {
-        --     ["<leader>f"]     = { name = "󰍉 File/Find" },
-        --     ["<leader>p"]     = { name = "󰏖 Packages" },
-        --     ["<leader>l"]     = { name = " LSP" },
-        --     ["<leader>u"]     = { name = " UI" },
-        --     ["<leader>b"]     = { name = "󰓩 Buffers" },
-        --     ["<leader>bs"]    = { name = "󰒺 Sort Buffers" },
-        --     ["<leader>d"]     = { name = " Debugger" },
-        --     ["<leader>g"]     = { name = "󰊢 Git" },
-        --     ["<leader>t"]     = { name = " Terminal" },
-        --     ["<leader><tab>"] = { name = "󰫍 Tabs" },
-        --     ["<leader>c"]     = { name = " Code" },
-        --     ["<leader>gh"]    = { name = " Hunks" },
-        --     ["<leader>q"]     = { name = "󱂬 Quit/Session" },
-        --     ["<leader>s"]     = { name = " Search" },
-        --     ["<leader>w"]     = { name = " Windows" },
-        --     ["<leader>x"]     = { name = " Diagnostics/Quickfix" },
-        --     ["<leader>k"]     = { name = " User" },
-        -- }
     },
     config = function(_, opts)
         require('which-key').setup(opts)
 
-        -- document existing key chains
-        require('which-key').register {
-            ['<leader>L'] = { name = 'location list', _ = 'which_key_ignore' },
-            ['<leader>O'] = { name = 'obsidian', _ = 'which_key_ignore' },
-            ['<leader>a'] = { name = 'ai', _ = 'which_key_ignore' },
-            ['<leader>b'] = { name = 'buffers', _ = 'which_key_ignore' },
-            ['<leader>d'] = { name = 'dap', _ = 'which_key_ignore' },
-            ['<leader>e'] = { name = 'explorer', _ = 'which_key_ignore' },
-            ['<leader>f'] = { name = 'find', _ = 'which_key_ignore' },
-            ['<leader>g'] = { name = 'git', _ = 'which_key_ignore' },
-            ['<leader>h'] = { name = 'harpoon', _ = 'which_key_ignore' },
-            ['<leader>l'] = { name = 'lsp', _ = 'which_key_ignore' },
-            ['<leader>n'] = { name = 'notes', _ = 'which_key_ignore' },
-            ['<leader>p'] = { name = 'packages', _ = 'which_key_ignore' },
-            ['<leader>q'] = { name = 'quit', _ = 'which_key_ignore' },
-            ['<leader>r'] = { name = 'terminal', _ = 'which_key_ignore' },
-            ['<leader>s'] = { name = 'split', _ = 'which_key_ignore' },
-            ['<leader>t'] = { name = 'tabs', _ = 'which_key_ignore' },
-            ['<leader>u'] = { name = 'user', _ = 'which_key_ignore' },
-            ['<leader>v'] = { name = 'options', _ = 'which_key_ignore' },
-            ['<leader>w'] = { name = 'write', _ = 'which_key_ignore' },
-            ['<leader>x'] = { name = 'quickfix', _ = 'which_key_ignore' },
+        require('which-key').add({
+            { "<leader>L",  group = "location list" },
+            { "<leader>O",  group = "obsidian" },
+            { "<leader>a",  group = "ai" },
+            { "<leader>b",  group = "buffers" },
+            { "<leader>d",  group = "dap" },
+            { "<leader>e",  group = "explorer" },
+            { "<leader>f",  group = "find" },
+            { "<leader>g",  group = "git" },
+            { "<leader>h",  group = "harpoon" },
+            { "<leader>l",  group = "lsp" },
+            { "<leader>n",  group = "notes" },
+            { "<leader>p",  group = "packages" },
+            { "<leader>q",  group = "quit" },
+            { "<leader>r",  group = "terminal" },
+            { "<leader>s",  group = "split" },
+            { "<leader>t",  group = "tabs" },
+            { "<leader>u",  group = "user" },
+            { "<leader>v",  group = "options" },
+            { "<leader>w",  group = "write" },
+            { "<leader>x",  group = "quickfix" },
         }
+        )
         --     -- register which-key VISUAL mode
         --     -- required for visual <leader>hs (hunk stage) to work
         --     require('which-key').register({
