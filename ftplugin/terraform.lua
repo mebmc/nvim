c40d26f8c7bb84997f07f9f2993f13decb45c748
Ben McDonald <ben@mebmc.uk>
2024-09-04 21:21:17 +0100

    Add config for terraform

---
+++ ftplugin/terraform.lua
@@ -0,0 +1,8 @@
+-- Set up auto formatting
+vim.cmd([[let g:terraform_fmt_on_save=1]])
+vim.cmd([[let g:terraform_align=1]])
+
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
+
+vim.opt.commentstring = "# %s"
