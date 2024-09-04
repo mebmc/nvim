943c3d052802f871d78a97d736aff37b50e546ee
Ben McDonald <ben@mebmc.uk>
2024-09-04 21:51:32 +0100

    Fix issue with yaml filetype detection

--- lua/core/polish.lua
+++ lua/core/polish.lua
@@ -1,32 +1,22 @@
--- local function yaml_ft(path, bufnr)
---   -- get content of buffer as string
---   local content = vim.filetype.getlines(bufnr)
---   if type(content) == "table" then
---     content = table.concat(content, "\n")
---   end
+local function yaml_ft(path, bufnr)
+  -- check if file is in roles, tasks, or handlers folder
+  local path_regex = vim.regex "(tasks\\|roles\\|handlers\\|plays\\|play\\.d)/"
+  if path_regex and path_regex:match_str(path) then
+    return "yaml.ansible"
+  end
 
---   -- check if file is in roles, tasks, or handlers folder
---   local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
---   if path_regex and path_regex:match_str(path) then
---     return "yaml.ansible"
---   end
---   -- check for known ansible playbook text and if found, return yaml.ansible
---   local regex = vim.regex "hosts:\\|tasks:"
---   if regex and regex:match_str(content) then
---     return "yaml.ansible"
---   end
+  -- return yaml if nothing else
+  return "yaml"
+end
 
---   -- return yaml if nothing else
---   return "yaml"
--- end
+vim.filetype.add {
+  extension = {
+    tf = "terraform",
+    yml = yaml_ft,
+    yaml = yaml_ft,
+    kdl = "kdl"
+  }
+}
 
--- vim.filetype.add {
---   extension = {
---     tf = "terraform",
---     yml = yaml_ft,
---     yaml = yaml_ft,
---     kdl = "kdl"
---   }
--- }
 
 vim.g.neovide_transparency = 0.9
