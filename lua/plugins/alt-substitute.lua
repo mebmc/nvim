a32d57e8330f484d2544669c7a77750e9751f4fc
Ben McDonald <ben@mebmc.uk>
2025-02-13 20:05:00 +0000

    Remove unused plugins

--- lua/plugins/alt-substitute.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "chrisgrieser/nvim-alt-substitute",
-    event = "CmdlineEnter",
-    -- lazy-loading with `cmd =` does not work well with incremental preview
-    opts = {
-        showNotification = true,
-    },
-}
--- lua/plugins/rust-tools.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"simrat39/rust-tools.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"neovim/nvim-lspconfig",
-		"mfussenegger/nvim-dap",
-	},
-	ft = "rust",
-}
