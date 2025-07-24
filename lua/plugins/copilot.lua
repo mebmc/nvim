905a71e5da0110b671fe234fb8ccc3086e75731a
Ben McDonald <ben@mebmc.uk>
2025-07-24 20:06:39 +0100

    Update codecompanion and copilot plugins

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,58 +1,62 @@
+-- return {}
 return {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "VeryLazy",
-    opts = {
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-        -- panel = {
-        --     enabled = false,
-        --     auto_refresh = true,
-        --     keymap = {
-        --         jump_prev = "[[",
-        --         jump_next = "]]",
-        --         accept = "<CR>",
-        --         refresh = "gr",
-        --         open = "<M-CR>"
-        --     },
-        --     layout = {
-        --         position = "right", -- | top | left | right
-        --         ratio = 0.4
-        --     },
-        -- },
-        -- suggestion = {
-        --     enabled = false,
-        --     auto_trigger = true,
-        --     debounce = 75,
-        --     keymap = {
-        --         accept = "<S-Tab>",
-        --         accept_word = "<C-s>",
-        --         accept_line = "<C-y>",
-        --         next = "<C-e>",
-        --         prev = "<C-w>",
-        --         dismiss = "<C-]>",
-        --     },
-        -- },
-        filetypes = {
-            yaml      = true,
-            markdown  = true,
-            help      = false,
-            gitcommit = true,
-            gitrebase = false,
-            hgcommit  = false,
-            svn       = false,
-            cvs       = false,
-            ["*"]     = true,
-        },
-        copilot_node_command = 'node', -- Node.js version must be > 16.x
-        server_opts_overrides = {
-            settings = {
-                advanced = {
-                    listCount = 3,          -- #completions for panel
-                    inlineSuggestCount = 3, -- #completions for getCompletions
-                }
-            },
-        },
-    }
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "VeryLazy",
+  opts = {
+    panel = { enabled = false },
+    -- panel = {
+    --   enabled = true,
+    --   auto_refresh = true,
+    --   keymap = {
+    --     jump_prev = "[[",
+    --     jump_next = "]]",
+    --     accept = "<CR>",
+    --     refresh = "gr",
+    --     open = "<C-P>"
+    --   },
+    --   layout = {
+    --     position = "right", -- | top | left | right
+    --     ratio = 0.4
+    --   },
+    -- },
+
+    -- suggestion = { enabled = false },
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      debounce = 75,
+      keymap = {
+        accept = "<Tab>",
+        accept_word = "<C-r>",
+        accept_line = "<C-e>",
+        next = "<C-n>",
+        prev = "<C-S-n>",
+        dismiss = "<Right>",
+      },
+    },
+    filetypes = {
+      yaml      = true,
+      markdown  = true,
+      help      = false,
+      gitcommit = true,
+      gitrebase = false,
+      hgcommit  = false,
+      svn       = false,
+      cvs       = false,
+      ["*"]     = true,
+    },
+    -- Node.js version must be > 20
+    copilot_node_command = vim.fn.expand("$HOME") .. "/.local/share/mise/installs/node/22/bin/node",
+    copilot_model = "gpt-4o-copilot",
+    server_opts_overrides = {
+      settings = {
+        advanced = {
+          listCount = 3,          -- #completions for panel
+          inlineSuggestCount = 3, -- #completions for getCompletions
+        }
+      },
+    },
+  }
 }
