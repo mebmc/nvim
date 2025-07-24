567770dba78f24bb7f4fb95f60908a50c3ad8d83
Ben McDonald <ben@mebmc.uk>
2025-07-24 21:14:22 +0100

    Update recorder config

--- lua/plugins/nvim-recorder.lua
+++ lua/plugins/nvim-recorder.lua
@@ -1,5 +1,22 @@
 return {
 	"chrisgrieser/nvim-recorder",
 	dependencies = "rcarriga/nvim-notify", -- optional
-	opts = {}, -- required even with default settings, since it calls `setup()`
+	opts = {
+		slots = { "x", "z" },
+
+		dynamicSlots = "static",
+
+		mapping = {
+			startStopRecording = "qr",
+			playMacro = "qf",
+			switchSlot = "qq",
+			editMacro = "qe",
+			deleteAllMacros = "qd",
+			yankMacro = "qy",
+			-- ⚠️ this should be a string you don't use in insert mode during a macro
+			addBreakPoint = "##",
+		},
+
+		clear = false,
+	},
 }
