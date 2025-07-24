428b1eac34ed32455af67e0229a1885ca029a87d
Ben McDonald <ben@mebmc.uk>
2025-07-24 20:06:39 +0100

    Update nvim config

--- lua/core/polish.lua
+++ lua/core/polish.lua
@@ -18,5 +18,28 @@ vim.filetype.add {
   }
 }
 
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.autoformat = false
+  else
+    vim.g.autoformat = false
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.autoformat = true
+  vim.g.autoformat = true
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
 
+-- Set the color for LSP Inlay Hints (Ghost Text)
+vim.api.nvim_set_hl(0, 'LspInlayHint', { fg = '#fC6370', bg = 'NONE' })
+-- vim.api.nvim_set_hl(0, 'CopilotGhostText', { fg = '#fC6370', bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'CopilotGhostText', { fg = '#4B8A5A', italic = true })
+
+-- Neovide settings
 vim.g.neovide_transparency = 0.9
